620e8e9468fa8004c4fb5e59ca656d46a0f4ef52
Eva María López Medina <157431634+evalm116@users.noreply.github.com>
2026-03-03 11:31:16 +0100

    Add files via upload

--- ScriptsAI/Steering/delegados/Face.cs
+++ ScriptsAI/Steering/delegados/Face.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class Face : Align
 {
-
     public override Steering GetSteering(AgentNPC character)
     {
         var direction = target.transform.position - character.Position;
         if (direction.magnitude == 0)
             return new Steering();
 
-        target.Orientation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;        
+        target.Orientation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
 
         return base.GetSteering(character);
     }
